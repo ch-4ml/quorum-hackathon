31c7441941ed58e509af57b2614e98057bce2642
ksheyon123 <52062612+ksheyon123@users.noreply.github.com>
2019-08-06 00:38:09 +0900

    hackathon_Prep.

--- router/main.js
+++ router/main.js
@@ -1,27 +1,21 @@
 var express = require('express');
-var router_main = express.Router();
+var router = express.Router();
 var fs = require('fs');
+var http = require('http');
+var app = express();
+var url = require('url');
 
-router_main.get('/', function (req, res) {
 
-    var template = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<p>
-<a href="/?id=item">상품</a>
-</p>
-<p>
-<a href="/?id=login">로그인</a>
-</p>
-</body>
-</html>`
-    res.end(template);
+router.get('/', function (req, res) {
+    res.render('index.html');
 });
 
-module.exports = router_main;
+router.get('/login', function (req, res) {
+    res.send('login')
+});
+
+router.get('/select_item', function (req, res) {
+   
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+// view 경로 설정
+app.set('views', __dirname + '/views');
+
+// 화면 engine을 ejs로 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// 기본 path를 /public으로 설정(css, javascript 등의 파일 사용을 위해)
+app.use(express.static(__dirname + '/public'));
+
 var mainRouter = require('./router/main');
 app.use(mainRouter);
 
