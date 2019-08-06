56edf9425c16f40134fc1fef7a1ef6853dc7901d
ksheyon123 <52062612+ksheyon123@users.noreply.github.com>
2019-08-07 01:31:52 +0900

    asd

--- dbconfig.js
+++ dbconfig.js
@@ -7,12 +7,3 @@ var dbConfig = {
 };
 
 module.exports = dbConfig;
-
-var dbItem = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'bankdb',
-};
-module.exports = dbItem;
