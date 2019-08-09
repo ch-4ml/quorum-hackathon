2221b73310f2698d04be9ff708ad03dc075c532a
ksheyon123 <52062612+ksheyon123@users.noreply.github.com>
2019-08-10 02:17:49 +0900

    ing_4

--- router/main.js
+++ router/main.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var http = require('http');
-var url = require('url');
-var router = express.Router();
 var mysql = require('mysql');
 var myConnection = mysql.createConnection(require('../dbConfig'));
 
@@ -36,90 +32,26 @@ router.get('/', function (req, res) {
 });
 
 router.get('/login', function (req, res) {
-    res.render('access_select_login.html');
-});
-
-router.post('/access_Selection', function (req, res) {
-    console.log(req.body.name)
-    if (req.body.name === '법인고객') {
-        var distinct_group = 'company';
-        var distinct_num = 1;
-        const data = {
-            dnum: distinct_num,
-            dgroup: distinct_group,
-        }
-        res.render('login.html', { data: data })
-
-    } else if (req.body.name === '일반고객') {
-        var distinct_group = 'client';
-        var distinct_num = 0;
-        const data = {
-            dnum: distinct_num,
-            dgroup: distinct_group,
-        }
-        res.render('login.html', { data: data })
-    } else if (req.body.name === '관리자') {
-        var distinct_group = 'bank';
-        var distinct_num = 2;
-        const data = {
-            dnum: distinct_num,
-            dgroup: distinct_group,
-        }
-        res.render('login.html', { data: data })
-    } else {
-        console.log('에러');
-    }
+    res.render('login.html');
 });
 
 router.post('/login_process', function (req, res) {
     const user = {
         insertedID: req.body.userID,
-        insertedPW: req.body.userPW,
-        distinct_num: req.body.distinct_num,
-        distinct_group: req.body.distinct_group,
-    }
-    /*session이 없을 경우 */
-    var sql = `select userID, userPW from testdb where groups = '${user.distinct_group}'`;
-    console.log(sql)
+        insertedPW: req.body.userPW
+    };
+    
+    var sql = `select * from testdb where userID = '${user.insertedID}' and userPW = '${user.insertedPW}'`;
     myConnection.query(sql, function (err, results) {
-        if (user.distinct_group === 'company') {
-            if (results[0].userID == user.insertedID && results[0].userPW == user.insertedPW) {
-                console.log('법인고객 Login');
-                req.session.user = {
-                    user: user.insertedID,
-                    dnum: user.distinct_num,
-                    dgrp: user.distinct_group,
-                }
-                res.redirect('/');
-            } else {
-                console.log('login 실패');
-            }
-        } else if (user.distinct_group === 'client') {
-            if (results[0].userID == user.insertedID && results[0].userPW == user.insertedPW) {
-                console.log('개인고객 Login');
-                req.session.user = {
-                    user: user.insertedID,
-                    dnum: user.distinct_num,
-                    dgrp: user.distinct_group,
-                }
-                res.redirect('/');
-            } else {
-                console.log('login 실패');
-            }
-        } else if (user.distinct_group === 'bank') {
-            if (results[0].userID == user.insertedID && results[0].userPW == user.insertedPW) {
-                console.log('관리자 Login');
-                req.session.user = {
-                    user: user.insertedID,
-                    dnum: user.distinct_num,
-                    dgrp: user.distinct_group,
-                }
-                res.redirect('/');  
-            } else {
-                console.log('login 실패');
-            }
+        if (results) {
+            req.session.user = {
+                user: results[0].userID,
+                id: results[0].id,
+                dnum: results[0].status
+            };
+            res.redirect('/');
         } else {
-            console.log("로그인 그룹 불명");
+            console.log("로그인 실패");
         }
     });
 });
@@ -138,66 +70,35 @@ router.get('/logout', function (req, res) {
 router.get('/register', function (req, res) {
     res.render('access_select_register.html');
 });
+
 router.post('/register_Selection', function (req, res) {
     console.log(req.body);
+    var data;
+    var distinct_num;
     if (req.body.name === '법인고객') {
-        var distinct_group = 'company';
-        var distinct_num = 1;
-        const data = {
-            dgroup: distinct_group,
-            dnum: distinct_num,
-        }
-        res.render('register.html', { data: data })
-
+        distinct_num = 1;
+        data = { dnum: distinct_num };
     } else if (req.body.name === '일반고객') {
-        var distinct_group = 'client';
-        var distinct_num = 0;
-        const data = {
-            dgroup: distinct_group,
-            dnum: distinct_num,
-        }
-        res.render('register.html', { data: data })
-    } else {
-        //은행 분류 코드
-        var distinct_group = 'bank';
-        var distinct_num = 2;
-        const data = {
-            dgroup: distinct_group,
-            dnum: distinct_num,
-        }
-        res.render('register.html', { data: data })
+        distinct_num = 2;
+        data = { dnum: distinct_num };
+    } else { // 은행
+        distinct_num = 0;
+        data = { dnum: distinct_num };
     }
+    res.render('register.html', { data: data })
 });
+
 router.post('/register_process', function (req, res) {
     console.log(req.body.distinct_num)
-    console.log(req.body)
-    if (req.body.distinct_num == 1) {
-        var userID = req.body.userID;
-        var userPW = req.body.userPW;
-        var groups = req.body.distinct_group;
-        var status = 1;
-        var sql = 'insert into testdb (userID, userPW, groups, status) values (?, ?, ?, ?)';
-        myConnection.query(sql, [userID, userPW, groups, status], function (err, data) {
-            if (err) {
-                console.log('Register Err :' + err);
-            }
-
-        });
-    } else if (req.body.distinct_num == 0) {
-        var userID = req.body.userID;
-        var userPW = req.body.userPW;
-        var groups = req.body.distinct_group;
-        var status = 0;
-        var sql = 'insert into testdb (userID, userPW, groups, status) values (?, ?, ?, ?)';
-        myConnection.query(sql, [userID, userPW, groups, status], function (err, data) {
-            if (err) {
-                console.log('Register Err :' + err);
-            }
-
-        });
-    } else {
-        console.log('Register Err ---');
-    }
+    var userID = req.body.userID;
+    var userPW = req.body.userPW;
+    var status = req.body.distinct_num;
+    var sql = 'insert into testdb (userID, userPW, status) values (?, ?, ?)';
+    myConnection.query(sql, [userID, userPW, status], function (err, data) {
+        if (err) {
+            console.log('Register Err :' + err);
+        }
+    });
     res.redirect('/');
 });
 module.exports = router;
--- router/select_item.js
+++ router/select_item.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var http = require('http');
-var url = require('url');
 var mysql = require('mysql');
 var myConnection = mysql.createConnection(require('../dbItem'));
-// var callFunc = require('../model/funcdir.js');
-
-
-//session 설정
-// app.use(session({secret: 'ssshhhhh'}));
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-// const loanData;
-
-// function setLoanData(data) {
-//     loanData = data;
-// }
-
-// function getLoanData() {
-//     return loanData;
-// }
 
 router.use(session({
     secret: 'keyboard cat',
@@ -36,225 +19,200 @@ myConnection.connect(function (err) {
     }
     console.log('MySQL Connection Complete_select_item');
 });
-//function
-var unique_array1 = [];
-var unique_array2 = [];
-function uniqueArray1(results) {
-    for (i = 0; i < results.length; i++) {
 
-        unique_array1[i] = results[i].investor
-
-    }
-    return unique_array1;
-}
-function uniqueArray2(results) {
-    for (i = 0; i < results.length; i++) {
-
-        unique_array2[i] = results[i].proposal
-
-    }
-    return unique_array2;
-}
-function fn_jsonArray1(investor_array1) {
-    var keyArray = new Array();
-    var investor = new Object();
-    for (i = 0; i < investor_array1.length; i++) {
-        if (investor_array1[i] != null && investor_array1[i] != undefined && investor_array1[i] != "") {
-            keyArray.push({ investor: investor_array1[i] });
-        }
-    }
-    return keyArray;
-}
-function fn_jsonArray2(investor_array2) {
-    var keyArray = new Array();
-    var investor = new Object();
-    for (i = 0; i < investor_array2.length; i++) {
-        if (investor_array2[i] != null && investor_array2[i] != undefined && investor_array2[i] != "") {
-            console.log(investor_array2[i])
-            keyArray.push({ proposal: investor_array2[i] });
-        }
-    }
-    return keyArray;
-}
-
-// var investedJson = fn_jsonArray();
-
-// function fn_jsonMake() {
-//     var keyArray = new Array();
-//     var investor = new Object();
-//     for (i = 0; i < investedJson.length; i++) {
-//         for (j = 0; j < results.length; j++) {
-//             // if (results[i] != null && results[i] != undefined && results[i] != "") {}
-//             if (investedJson[i].investor == results[j].investor) {
-//                 console.log(investedJson[0].investor)
-//                 console.log(results[2].investor)
-//                 keyArray.add({ item: results[j].item });
-
-
-
-//             }
-//         }
-//     }
-//     console.log(keyArray)
-//     return keyArray;
-// }
-// var investJson = fn_jsonMake();
-// console.log(investJson)
-
-
+//수정
 router.get('/selected_item', function (req, res) {
-    console.log(req.body)
-    var sql = 'select item, category from bankdb where status = 0';
+    var sql = 'select id, item, category, val from bankdb where status = 1';
     myConnection.query(sql, function (err, results) {
         if (err) {
             console.log('bankdb ' + err);
         }
-        data_send = {
-            item_name: results,
-            item_leng: results.length,
-            userData: req.session.user,
+        var data = {
+            bonds: results,
+            userData: req.session.user
         }
-        res.render('select_item.html', { data: data_send })
+        res.render('select_item.html', { data: data });
     });
 });
 
 router.post('/select_process', function (req, res) {
-    console.log(req.body)
-    console.log('Select_process 접속')
-    var list = "";
-    function callSum(req) {
-        var i = 0;
-        while (i < req.body.item.length) {
-            list = list + `item = ` + `"${req.body.item[i]}"`;
-            if (i != req.body.item.length - 1) {
-                list = list + ` OR `
-            }
-            i++;
-        }
-        return list;
+    console.log(req.body.min)
+    var stmt = "";
+    const id = req.body.item;
+    const min = parseInt(req.body.min);
+    const max = parseInt(req.body.max);
+    for (i = 0; i < req.body.item.length; i++) {
+        stmt = stmt + `id = ${id[i]}`;
+        if (i < req.body.item.length - 1) stmt = stmt + " OR ";
     }
-    var condition = callSum(req);
-    var sql = `SELECT * from bankdb where val between ${req.body.min} AND ${req.body.max} AND (${condition})  AND (status = 0)`;
-    console.log(sql)
+    var sql = `select id, item, category, val from bankdb where (val between ${min} AND ${max}) AND (` + stmt + ')';
+    console.log(sql);
     myConnection.query(sql, function (err, results) {
         if (err) {
             console.log('bankdb ' + err);
         }
-        loan_data = {
-            item_data: results,
-            userData: req.session.user,
+        data = {
+            bonds: results,
+            userData: req.session.user
         }
-        res.render('item_list.html', { data: loan_data })
+        res.render('item_list.html', { data: data })
     });
 
 });
 
 router.post('/sell_item', function (req, res) {
-    console.log('bankdb변경 & company db로 전달')
-    console.log(req.session.user.user);
-    var retrieved_data = JSON.parse(req.body.selected_item_list);
-    enrolled_all = {
-        enrolled_item: retrieved_data,
-        enrolled_value: req.body.total_item_value,
-        value_proposal: req.body.proposal,
-        investor: req.session.user.user
+    console.log(req.body)
+    var stmt = "";
+    const id = req.body.item;
+    for (i = 0; i < req.body.item.length; i++) {
+        stmt = stmt + `id = ${id[i]}`;
+        if (i < req.body.item.length - 1) stmt = stmt + " OR ";
     }
-    function callSum(enrolled_all) {
-        var list = ""
-        var i = 0;
-        while (i < enrolled_all.enrolled_item.length) {
-            list = list + `item = ` + `"${enrolled_all.enrolled_item[i].item}"`;
-            if (i != enrolled_all.enrolled_item.length - 1) {
-                list = list + ` OR `
-            }
-            i++;
-        }
-        return list;
-    }
-    var condition = callSum(enrolled_all);
-    var sql = `UPDATE bankdb SET status = 1, investor = "${enrolled_all.investor}", proposal = ${enrolled_all.value_proposal} where ${condition}`;
+    var sql = `UPDATE bankdb SET status = 2, investor = "${req.session.user.id}", proposal = ${req.body.proposal} where ${stmt}`;
     myConnection.query(sql, function (err, results) {
         if (err) {
             console.log('bankdb update ' + err);
         }
         console.log(results);
     });
-
-    res.redirect('/')
+    res.redirect('/');
 });
 
+//수정
 router.get('/item_proposal', function (req, res) {
-    var sql_1 = `SELECT investor, proposal FROM bankdb WHERE status=1 GROUP BY investor, proposal`;
+    var sql_1 = `SELECT investor, proposal FROM bankdb WHERE status=2 GROUP BY investor, proposal`;
     myConnection.query(sql_1, function (err, results_1) {
         if (err) {
 
         }
-        var sql_2 = `SELECT item, val, category, investor, proposal FROM bankdb WHERE status = 1`;
+        // 수정
+        var sql_2 = `SELECT id, item, val, category, investor, proposal FROM bankdb WHERE status = 2`;
         myConnection.query(sql_2, function (err, results_2) {
             if (err) {
                 console.log('bankdb_investor load ' + err)
             }
-            data_packet = {
+            data = {
                 investData: results_1,
-                loanData : results_2,
-                userData : req.session.user,
-            }
+                loanData: results_2,
+                userData: req.session.user
+            };
 
-            res.render('item_proposal.html', {data:data_packet})
+            res.render('item_proposal.html', { data: data })
         });
-
     });
 });
-
+//매입 기관의 채권 구매 신청
 router.post('/build_item', function (req, res) {
-    var loaning = JSON.parse(req.body.loanedData)
-    console.log(loaning)
-    console.log(req.body.investedData)
-    function callSum(enrolled_all) {
-        var list = ""
-        var i = 0;
-        while (i < enrolled_all.enrolled_item.length) {
-            list = list + `item = ` + `"${enrolled_all.enrolled_item[i].item}"`;
-            if (i != enrolled_all.enrolled_item.length - 1) {
-                list = list + ` OR `
-            }
-            i++;
-        }
-        return list;
+
+    var stmt = "";
+    const id = req.body.loanedData;
+    for (i = 0; i < req.body.loanedData.length; i++) {
+        stmt = stmt + `id = ${id[i]}`;
+        if (i < req.body.loanedData.length - 1) stmt = stmt + " OR ";
     }
-    var condition = callSum(enrolled_all);
-    var sql = `UPDATE bankdb SET status = 2, creditor = ${req.session.user.id} where ${condition}`
-    myConnection.query(sql, function(err, results) {
-        if(err) {
+    var sql = `UPDATE bankdb SET status = 3, requestor = ${req.body.user.id} where ${stmt}`
+    myConnection.query(sql, function (err, results) {
+        if (err) {
             console.log('item unselled ' + err);
         }
         res.redirect('/');
     });
 });
-router.get('/item_enroll')
+router.get('/item_enroll', function (req, res) {
+    var sql = `SELECT * FROM bankdb WHERE status=0`;
+    myConnection.query(sql, function (err, results) {
+        if (err) {
+            console.log('bankdb 전체 Data Load 실패');
+        }
+        data = {
+            bonds: results,
+            userData: req.session.user,
+        }
+        res.render('all_item.html', { data: data });
+    });
+});
+router.post('/show_item', function (req, res) {
+    console.log(req.body.item)
+    var stmt = "";
+    const id = req.body.item;
+    for (i = 0; i < req.body.item.length; i++) {
+        stmt = stmt + `id = ${id[i]}`;
+        if (i < req.body.item.length - 1) stmt = stmt + " OR ";
+    }
+    var sql = `UPDATE bankdb SET status = 1 where ${stmt}`
+    myConnection.query(sql, function (err, results) {
+        if (err) {
+            console.log('item unselled ' + err);
+        }
+        res.redirect('/');
+    });
+    console.log('채권 블록체인 등록')
+});
+router.get('/sell_confirm', function(req, res) {
+    var sql = `SELECT * FROM bankdb WHERE status = 3`;
+    myConnection.query(sql, function(err, results) {
+        if(err) {
+            console.log('매입기관 허가 절차 Err')
+        }
+        data ={
+            bonds:results,
+            userData:req.session.user
+        }
+        res.render('confirm.html', {data:data});
+    });
+});
 
+router.post('/confirm_complete', function(req, res) {
+    console.log(req.body)
+    var stmt = "";
+    const id = req.body.item;
+    for (i = 0; i < req.body.item.length; i++) {
+        stmt = stmt + `id = ${id[i]}`;
+        if (i < req.body.item.length - 1) stmt = stmt + " OR ";
+    }
+    var sql = `UPDATE bankdb SET status = 4 where ${stmt}`
+    myConnection.query(sql, function (err, results) {
+        if (err) {
+            console.log('confirmation_err ' + err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/item_sell_to_investor', function(req, res) {
+    var sql = `SELECT id, item, category, val, proposal, investor, creditor FROM bankdb WHERE status = 4`;
+    myConnection.query(sql, function(err, results) {
+        if(err) {
+            console.log('Data from bank to company Err');
+        }
+        data = {
+            bonds:results,
+            userData:req.session.user
+        }
+        res.render('sell_to_client.html', {data:data})
+    });
+});
+
+router.post('/confirm', function(req, res) {
+    console.log(req.body)
+    var stmt = "";
+    const id = req.body.item;
+    for (i = 0; i < req.body.item.length; i++) {
+        stmt = stmt + `id = ${id[i]}`;
+        if (i < req.body.item.length - 1) stmt = stmt + " OR ";
+    }
+    var sql = `UPDATE bankdb SET com_proposal= ${req.body.com_proposal} where id = ${req.body.item}`;
+    console.log(sql)
+    myConnection.query(sql, function(err, results) {
+        if(err) {
+            console.log('매입기관에서 투자자 채권 판매 Err');
+        }
+        data = {
+            bonds:results,
+            userData:req.session.user
+        }
+        res.render('contract.html', {data:data})
+    });
+});
 
 module.exports = router;
-
-// 중복 투자자 제거 함수
-        // var unique_index = uniqueArray1(results);
-        // var investor_array1 = unique_index.filter(function (value, index, self) {
-        //     return self.indexOf(value) === index;
-        // });
-        // var unique_index = uniqueArray2(results);
-        // var investor_array2 = unique_index.filter(function (value, index, self) {
-        //     return self.indexOf(value) === index;
-        // });
-        // var investedJson = fn_jsonArray1(investor_array1);
-        // var investedPro = fn_jsonArray2(investor_array2);
-
-        // console.log(data_packet.investData)
-        // console.log(data_packet.loanData)
-        // var data_3 = JSON.stringify(data_packet.investData)
-        // var data_4 = JSON.stringify(data_packet.loanData)
-        // console.log(data_3)
-        // console.log(data_4)
-        // var data_5 = JSON.parse(data_3)
-        // var data_6 = JSON.parse(data_4)
-        // console.log(data_5)
-        // console.log(data_6)
